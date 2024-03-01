02803ad1899202427884da8a1cd7044567870fa1
Rizanazer <ichurizan123@gmail.com>
2024-03-01 23:43:17 +0530

    profile

--- src/pages/Home/Profile/ProfileScreen.jsx
+++ src/pages/Home/Profile/ProfileScreen.jsx
@@ -1,11 +1,25 @@
-function ProfileScreen(){
-    return(
-      <>
-      <div className="section1 section_margin box"></div>
-      <div className="section2">
-        profile screen
+import "./Profile.css"
+function ProfileScreen() {
+  return (
+    <>
+      <div className="h_w_full flex flexrow  zindex2 profile_whole">
+        <div className="triangle flex"></div>
+        <div className="box profilesection flex flexrow center">
+
+          <div className="section2 profilesection1 flex flexcolumn">
+            <div className="profile_photosection center">
+            <img src="images/profilepic.jpg" alt="image" className="circle profilemain_photo profile_chat_img" />
+            </div>
+            <div className="profile_postsection">
+            <div className="box h_w_full"></div>
+            </div>
+            
+          </div>
+          <div className="section1 profilesection2"></div>
+        </div>
       </div>
-      </>
-    );
-  }
-  export default ProfileScreen;
+
+    </>
+  );
+}
+export default ProfileScreen;
