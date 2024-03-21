a139f10b39f36527db33b8571f20576b373b9b0e
Rizanazer <ichurizan123@gmail.com>
2024-03-21 17:42:12 +0530

    commit

--- src/pages/Home/Nav/Nav.jsx
+++ src/pages/Home/Nav/Nav.jsx
@@ -3,15 +3,15 @@ import { MdGroups, MdOutlineSettings, MdPersonAddAlt1} from "react-icons/md";
 import { PiChat, PiChats } from "react-icons/pi";
 import UserProfile from "../Functions/UserProfile";
 
-export default function Nav({Screen,setScreen,setSetting}){
+export default function Nav({Screen,setScreen,setSetting,setviewchat}){
     return(<aside className="nav center">
         <div className="navbox center">
           {/* <CgProfile className="icon" color={Screen=="Profile" ? "#979697" :"#fff"} onClick={() => { setScreen("Profile"); }} /> */}
-          <PiChat className="icon" color={Screen=="PersonalMessage" ? "#979697" :"#fff"} onClick={() => { setScreen("PersonalMessage");}} />
-          <PiChats className="icon" color={Screen=="CommunityMessage" ? "#979697" :"#fff"} onClick={() => { setScreen("CommunityMessage");}} />
-          <CgSearch className="icon" color={Screen=="SearchCommunity" ? "#979697" :"#fff"} onClick={() => { setScreen("SearchCommunity");}} />
-          <MdPersonAddAlt1 className="icon" color={Screen=="AddFriends" ? "#979697" :"#fff"} onClick={() => { setScreen("AddFriends");}} />
-          <MdOutlineSettings className="icon" color={Screen=="Settings" ? "#979697" :"#fff"} onClick={() => {setSetting()}} />
+          <PiChat className="icon" color={Screen=="PersonalMessage" ? "#979697" :"#fff"} onClick={() => { setScreen("PersonalMessage");setviewchat(false)}} />
+          <PiChats className="icon" color={Screen=="CommunityMessage" ? "#979697" :"#fff"} onClick={() => { setScreen("CommunityMessage");setviewchat(false)}} />
+          <CgSearch className="icon" color={Screen=="SearchCommunity" ? "#979697" :"#fff"} onClick={() => { setScreen("SearchCommunity");setviewchat(false)}} />
+          <MdPersonAddAlt1 className="icon" color={Screen=="AddFriends" ? "#979697" :"#fff"} onClick={() => { setScreen("AddFriends");setviewchat(false)}} />
+          <MdOutlineSettings className="icon" color={Screen=="Settings" ? "#979697" :"#fff"} onClick={() => {setSetting();setviewchat(false)}} />
         </div>
 
         <UserProfile />
